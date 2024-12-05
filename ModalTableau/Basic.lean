6d4e60f9bb3d6f71473243390d5c91a6969d09d8
SnO2WMaN <me@sno2wman.net>
2024-12-05 09:37:54 +0900

    Labelled Gentzen, soundness

--- ModalTableau/Basic.lean
+++ ModalTableau/Basic.lean
@@ -1,210 +1,66 @@
 import Foundation.Modal.NNFormula
+import Foundation.Modal.Kripke.Basic
 
 namespace LO.Modal
 
-open NNFormula
 
-namespace Tableau
+namespace Labelled
+
+
+abbrev Label := ℕ
+
+namespace Label
+
+def Assignment (M : Kripke.Model) := Label → M.World
+
+end Label
+
+
+abbrev LabelTerm := Label × Label
+
+namespace LabelTerm
+
+def evaluated {M : Kripke.Model} (f : Label.Assignment M) : LabelTerm → Prop := λ ⟨x, y⟩ => M.Rel (f x) (f y)
+
+end LabelTerm
 
 
 structure LabelledFormula where
-  label : ℕ
-  formula : NNFormula ℕ
+  label : Label
+  formula : Formula ℕ
+deriving DecidableEq, Repr
 
 namespace LabelledFormula
 
-notation:max l " ∶ " φ => LabelledFormula.mk l φ
+notation:95 x " ∶ " φ => LabelledFormula.mk x φ
 
-section toStr
+def Satisfies (M : Kripke.Model) (f : Label.Assignment M) : LabelledFormula → Prop := λ (x ∶ φ) => (f x) ⊧ φ
 
-def toStr (lf : LabelledFormula) : String := s!"{lf.label} : {lf.formula.toStr}"
+namespace Satisfies
 
-instance : Repr (LabelledFormula) := ⟨λ t _ => toStr t⟩
-instance : ToString (LabelledFormula) := ⟨toStr⟩
-
-#eval (0 ∶ (atom 1 ➝ atom 2))
-
-end toStr
+protected instance semantics {M : Kripke.Model} : Semantics (LabelledFormula) (Label.Assignment M) := ⟨fun x ↦ LabelledFormula.Satisfies M x⟩
 
 
-section deq
+variable {M : Kripke.Model} {f : Label.Assignment M}
+variable {x y : Label}
+variable {φ ψ : Formula ℕ} {xφ: LabelledFormula}
 
-def deq (lf₁ lf₂ : LabelledFormula) : Bool := lf₁.formula = lf₂.formula
-instance : BEq LabelledFormula := ⟨deq⟩
+@[simp] protected lemma iff_models : f ⊧ (x ∶ φ) ↔ f x ⊧ φ := by tauto;
 
-end deq
+lemma imp_def : f ⊧ (x ∶ φ ➝ ψ) ↔ (f x) ⊧ φ ➝ ψ := by tauto;
 
+lemma box_def : f ⊧ (x ∶ □φ) ↔ (f x) ⊧ □φ := by tauto;
+
+end Satisfies
 
 end LabelledFormula
 
 
+structure LabelledNNFormula where
+  label : Label
+  formula : NNFormula ℕ
+deriving DecidableEq, Repr
 
-abbrev RelTerm := ℕ × ℕ
-
-namespace RelTerm
-
-notation a " ≺ " b => (a, b)
-
-section toStr
-
-def toStr (rt : RelTerm) : String := s!"{rt.1} ≺ {rt.2}"
-instance : Repr RelTerm := ⟨λ t _ => toStr t⟩
-instance : ToString RelTerm := ⟨toStr⟩
-
-end toStr
-
-end RelTerm
-
-
-
-abbrev RelTerms := List RelTerm
-
-namespace RelTerms
-
-abbrev rel (rs : RelTerms) := rs.contains
-
-end RelTerms
-
-
-structure GenFormula where
-  formula : LabelledFormula
-  generated : List LabelledFormula
-deriving Repr, BEq
-
-
-structure Path where
-  path : List GenFormula
-  rels : List (ℕ × ℕ)
-deriving Repr
-
-namespace Path
-
-section toStr
-
--- def toStr (P : Path) : String := P.map (λ ⟨lφ, g⟩ => s!"{lφ} => {g}\n") |> String.join
-
--- instance : Repr Path := ⟨λ t _ => toStr t⟩
-
-#eval (⟨[
-    ⟨(0 ∶ atom 1), []⟩,
-    ⟨(0 ∶ atom 1 ⋏ atom 2), []⟩
-  ], []⟩ : Path)
-
-def insert_gf (gf : GenFormula) : Path → Path := λ ⟨path, rels⟩ => ⟨path.insert gf, rels⟩
-
-end toStr
-
-partial def applyRules₁ : Path → List (Path) := λ ⟨nodes, rels⟩ =>
-  match nodes with
-  | [] => [⟨[], rels⟩]
-  | ⟨l ∶ atom p, []⟩ :: rest =>
-    applyRules₁ ⟨rest, rels⟩ |>.map $ insert_gf ⟨l ∶ atom p, [l ∶ atom p]⟩
-  | ⟨l ∶ natom p, []⟩ :: rest =>
-    applyRules₁ ⟨rest, rels⟩ |>.map $ insert_gf ⟨l ∶ natom p, [l ∶ natom p]⟩
-  | ⟨l ∶ φ ⋏ ψ, []⟩ :: rest =>
-    applyRules₁ (⟨rest |>.insert ⟨l ∶ ψ, []⟩ |>.insert ⟨l ∶ φ, []⟩, rels⟩) |>.map $ insert_gf ⟨l ∶ φ ⋏ ψ, [l ∶ φ, l ∶ ψ]⟩
-  | ⟨l ∶ φ ⋎ ψ, []⟩ :: rest =>
-    (applyRules₁ ⟨rest.insert ⟨l ∶ φ, []⟩, rels⟩ |>.map $ insert_gf ⟨l ∶ φ ⋎ ψ, [l ∶ φ]⟩)
-    ++
-    (applyRules₁ ⟨rest.insert ⟨l ∶ ψ, []⟩, rels⟩ |>.map $ insert_gf ⟨l ∶ φ ⋎ ψ, [l ∶ ψ]⟩)
-  | ⟨l ∶ ◇φ, []⟩ :: rest =>
-    letI m := (l :: rest.map (λ ⟨⟨l, _⟩, _⟩ => l) ++ (rels.map (λ ⟨x, y⟩ => [x, y]) |>.flatten) |>.foldl max 0) + 1
-    applyRules₁ ⟨rest.insert ⟨m ∶ φ, []⟩, rels.insert (l, m)⟩ |>.map $ insert_gf ⟨l ∶ ◇φ, [m ∶ φ]⟩
-  | gφ :: rest =>
-    applyRules₁ ⟨rest, rels⟩ |>.map $ insert_gf gφ
-
-#eval applyRules₁ ⟨[
-    ⟨0 ∶ ∼(□(atom 0 ➝ atom 1) ➝ □(atom 0) ➝ □(atom 1)), []⟩,
-  ], []⟩
-
-#eval applyRules₁ ⟨[
-    ⟨0 ∶ natom 1 ⋏ (atom 2 ⋏ atom 3), []⟩,
-    ⟨0 ∶ atom 4 ⋏ atom 5, []⟩,
-    ⟨0 ∶ atom 1, []⟩,
-  ], []⟩
-
-
-#eval applyRules₁ ⟨[
-    ⟨0 ∶ atom 1 ⋎ (atom 2 ⋏ atom 3), []⟩,
-    ⟨0 ∶ atom 4 ⋏ atom 5, []⟩,
-    ⟨0 ∶ atom 1, []⟩,
-  ], []⟩
-
-#eval applyRules₁ ⟨[
-    ⟨0 ∶ ◇(atom 0 ⋏ natom 1), []⟩,
-  ], []⟩
-
-#eval applyRules₁ ⟨[
-    ⟨0 ∶ ◇(atom 1), []⟩,
-    ⟨1 ∶ ◇(atom 0 ⋏ ◇(natom 1)), []⟩,
-    ⟨2 ∶ ◇(atom 2), []⟩,
-  ], []⟩
-
-#eval (applyRules₁ ⟨[
-    ⟨0 ∶ ◇◇◇(atom 1 ⋏ □(atom 2)), []⟩,
-    ⟨0 ∶ ◇◇◇(atom 3 ⋏ □(atom 4)), []⟩,
-  ], []⟩)
-
-
-partial def applyRules₂ : Path → Path := λ ⟨nodes, rels⟩ =>
-  match nodes with
-  | [] => ⟨[], rels⟩
-  | ⟨l ∶ □φ, c⟩ :: rest =>
-    letI tos : List LabelledFormula := rels
-      |>.filter (λ ⟨f, _⟩ => f = l)
-      |>.map (λ ⟨_, t⟩ => t ∶ φ)
-      |>.filter (!c.contains ·)
-    let ges : List GenFormula := tos.map (λ t => ⟨t, []⟩)
-    applyRules₂ (⟨rest ++ ges, rels⟩) |>.insert_gf ⟨l ∶ □φ, c ++ tos⟩
-    -- Path.insert_gf ⟨l ∶ □φ, c ++ tos⟩ (⟨rest ++ ps, rels⟩)
-  | gφ :: rest =>
-    applyRules₂ ⟨rest, rels⟩ |>.insert_gf gφ
-
-#eval (applyRules₂ ⟨[
-    ⟨0 ∶ □(atom 1 ⋏ □(atom 2)), []⟩,
-  ], [(0, 1), (0, 2)]⟩)
-
-#eval (applyRules₂ ⟨[
-    ⟨0 ∶ □(atom 1 ⋏ □(atom 2)), []⟩,
-  ], [(0, 1), (0, 2), (0, 3), (2, 0)]⟩)
-
-#eval (applyRules₁ $ applyRules₂ ⟨[
-    ⟨0 ∶ □(atom 1 ⋏ □(natom 2)), []⟩,
-  ], [(0, 1), (0, 2), (0, 3), (2, 0)]⟩) |>.map applyRules₂
-
-#eval (applyRules₂ ⟨[
-    ⟨0 ∶ □(atom 0), []⟩,
-    ⟨2 ∶ □(atom 1 ⋏ □(natom 2)), []⟩,
-  ], [(2, 0), (0, 2), (0, 1)]⟩)
-
-#eval (applyRules₁ $ (applyRules₂ ⟨[
-    ⟨0 ∶ □(atom 0), []⟩,
-    ⟨2 ∶ □(atom 1 ⋏ □(natom 2)), []⟩,
-  ], [(2, 0), (0, 2), (0, 1)]⟩)) |>.map applyRules₂
-
-#eval (⟨[
-    ⟨0 ∶ ∼(□(atom 0 ➝ atom 1) ➝ □(atom 0) ➝ □(atom 1)), []⟩,
-  ], []⟩ : Path) |>.applyRules₁ |>.map applyRules₂ |> (List.map applyRules₁)
-
-abbrev applyRules := List.flatten ∘ (List.map (applyRules₁ ∘ applyRules₂))
-
-
-#eval applyRules $ applyRules $ applyRules $ applyRules [(⟨[
-    ⟨0 ∶ ∼(□(atom 0 ➝ atom 1) ➝ □(atom 0) ➝ □(atom 1)), []⟩,
-  ], []⟩ : Path)]
-
-
-def isClosed (P : Path) : Bool :=
-  let lφs := P.path.map (λ ⟨lφ, _⟩ => lφ)
-  lφs.any (λ ⟨l, φ⟩ => lφs.contains ⟨l, ∼φ⟩)
-
-#eval applyRules [(⟨[⟨0 ∶ ∼(□(atom 0 ➝ atom 1) ➝ □(atom 0) ➝ □(atom 1)), []⟩], []⟩ : Path)]
-    |>.map (λ P => P.isClosed)
-
-end Path
-
-
-
-end Tableau
+end Labelled
 
 end LO.Modal
---
+++ ModalTableau/Gentzen.lean
@@ -0,0 +1,212 @@
+import Foundation.Modal.Kripke.Basic
+import ModalTableau.Basic
+
+namespace LO.Modal
+
+namespace Labelled
+
+namespace Gentzen
+
+open Formula
+
+structure SequentPart where
+  fmls : List LabelledFormula
+  rels : List LabelTerm
+deriving Repr
+
+namespace SequentPart
+
+def isFreshLabel (x : Label) (Γ : SequentPart) : Prop := (x ∉ Γ.fmls.map LabelledFormula.label) ∧ (∀ y, (x, y) ∉ Γ.rels) ∧ (∀ y, (y, x) ∉ Γ.rels)
+
+variable {x : Label} {Γ : SequentPart}
+
+lemma not_include_labelledFml_of_isFreshLabel (h : Γ.isFreshLabel x) : ∀ φ, (x ∶ φ) ∉ Γ.fmls := by have := h.1; aesop;
+
+lemma not_include_relTerm_of_isFreshLabel₁ (h : Γ.isFreshLabel x) : ∀ y, (x, y) ∉ Γ.rels := by have := h.2; aesop;
+
+lemma not_include_relTerm_of_isFreshLabel₂ (h : Γ.isFreshLabel x) : ∀ y, (y, x) ∉ Γ.rels := by have := h.2.2; aesop;
+
+end SequentPart
+
+
+structure Sequent where
+  pre : SequentPart
+  pos : SequentPart
+deriving Repr
+
+infix:50 " ⟹ " => Sequent.mk
+
+namespace Sequent
+
+abbrev Satisfies (M : Kripke.Model) (f : Label.Assignment M) : Sequent → Prop := λ ⟨Γ, Δ⟩ =>
+  (∀ lφ ∈ Γ.fmls, f ⊧ lφ) ∧ (∀ r ∈ Γ.rels, r.evaluated f) →
+  (∃ lφ ∈ Δ.fmls, f ⊧ lφ) ∨ (∃ r ∈ Δ.rels, r.evaluated f)
+
+namespace Satisfies
+
+protected instance semantics {M : Kripke.Model} : Semantics Sequent (Label.Assignment M) := ⟨fun x ↦ Satisfies M x⟩
+
+end Satisfies
+
+end Sequent
+
+
+inductive Derivation : Sequent → Type _
+| ax {Γ Δ : SequentPart} {x} {a} : Derivation (⟨⟨x, .atom a⟩ :: Γ.fmls, Γ.rels⟩ ⟹ ⟨⟨x, .atom a⟩ :: Δ.fmls, Δ.rels⟩)
+-- | bot {Γ Δ : SequentPart} {x} : Derivation (Γ ⟹ ⟨⟨x, ⊥⟩ :: Δ.fmls, Δ.rels⟩)
+| impL {Γ Δ : SequentPart} {x} {φ ψ} :
+    Derivation (⟨Γ.fmls, Γ.rels⟩ ⟹ ⟨⟨x, φ⟩ :: Δ.fmls, Δ.rels⟩) →
+    Derivation (⟨⟨x, ψ⟩ :: Γ.fmls, Γ.rels⟩ ⟹ ⟨Δ.fmls, Δ.rels⟩) →
+    Derivation (⟨⟨x, φ ➝ ψ⟩ :: Γ.fmls, Γ.rels⟩ ⟹ ⟨Δ.fmls, Δ.rels⟩)
+| impR {Γ Δ : SequentPart} {x} {φ ψ} :
+    Derivation (⟨⟨x, φ⟩ :: Γ.fmls, Γ.rels⟩ ⟹ ⟨⟨x, ψ⟩ :: Δ.fmls, Δ.rels⟩) →
+    Derivation (⟨Γ.fmls, Γ.rels⟩ ⟹ ⟨⟨x, φ ➝ ψ⟩ :: Δ.fmls, Δ.rels⟩)
+| boxL {Γ Δ : SequentPart} {x y} {φ} :
+    Derivation (⟨⟨x, □φ⟩ :: ⟨y, φ⟩ :: Γ.fmls, ⟨x, y⟩ :: Γ.rels⟩ ⟹ ⟨Δ.fmls, Δ.rels⟩) →
+    Derivation (⟨⟨x, □φ⟩ :: Γ.fmls, ⟨x, y⟩ :: Γ.rels⟩ ⟹ ⟨Δ.fmls, Δ.rels⟩)
+| boxR {Γ Δ : SequentPart} {x y} {φ} :
+    x ≠ y → Γ.isFreshLabel y → Δ.isFreshLabel y →
+    Derivation (⟨Γ.fmls, ⟨x, y⟩ :: Γ.rels⟩ ⟹ ⟨⟨y, φ⟩ :: Δ.fmls, Δ.rels⟩) →
+    Derivation (⟨Γ.fmls, Γ.rels⟩ ⟹ ⟨⟨x, □φ⟩ :: Δ.fmls, Δ.rels⟩)
+/-
+| diaL {Γ Δ : SequentPart} {x y} {φ} :
+    x ≠ y → Γ.isFreshLabel y → Δ.isFreshLabel y →
+    Derivation (⟨⟨x, φ⟩ :: Γ.fmls, ⟨x, y⟩ :: Γ.rels⟩ ⟹ ⟨Δ.fmls, Δ.rels⟩) →
+    Derivation (⟨⟨x, ◇φ⟩ :: Γ.fmls, Γ.rels⟩ ⟹ ⟨Δ.fmls, Δ.rels⟩)
+| diaR {Γ Δ : SequentPart} {x y} {φ} :
+    Derivation (⟨Γ.fmls, ⟨x, y⟩ :: Γ.rels⟩ ⟹ ⟨⟨x, ◇φ⟩ :: ⟨y, φ⟩ :: Δ.fmls, Δ.rels⟩) →
+    Derivation (⟨Γ.fmls, ⟨x, y⟩ :: Γ.rels⟩ ⟹ ⟨⟨x, ◇φ⟩ :: Δ.fmls, Δ.rels⟩)
+-/
+prefix:70 "⊢ᵍ " => Derivation
+
+
+abbrev Derivable (S : Sequent) : Prop := Nonempty (⊢ᵍ S)
+prefix:70 "⊢ᵍ! " => Derivable
+
+lemma soundness {S : Sequent} : ⊢ᵍ S → ∀ M, ∀ f, S.Satisfies M f := by
+  intro d;
+  induction d with
+  | ax =>
+    rintro M f ⟨hΓ, hX⟩;
+    simp_all;
+  | @impL Γ Δ x φ ψ d₁ d₂ ih₁ ih₂ =>
+    rintro M f ⟨hΓ, hX⟩;
+    have ⟨hΓ₁, hΓ₂⟩ : f ⊧ (x ∶ φ ➝ ψ) ∧ ∀ a ∈ Γ.fmls, f ⊧ a := by simpa using hΓ;
+    replace hX : ∀ x y, ⟨x, y⟩ ∈ Γ.rels → LabelTerm.evaluated f ⟨x, y⟩ := by simpa using hX;
+    have : ¬(f x ⊧ φ) ∨ (f x ⊧ ψ) := by
+      simpa [LabelledFormula.Satisfies.imp_def, Semantics.Imp.realize_imp, imp_iff_not_or] using hΓ₁;
+    rcases this with (_ | h);
+    . replace ih₁ :
+        (∀ lφ ∈ Γ.fmls, f ⊧ lφ) →
+        (∀ a b, (a, b) ∈ Γ.rels → LabelTerm.evaluated f (a, b)) →
+        ((f x) ⊧ φ ∨ ∃ a ∈ Δ.fmls, f ⊧ a) ∨ ∃ a b, (a, b) ∈ Δ.rels ∧ LabelTerm.evaluated f (a, b)
+        := by simpa [Sequent.Satisfies] using ih₁ M f;
+      rcases ih₁ hΓ₂ hX with (_ | ⟨lψ, _, _⟩) | _;
+      . simp_all;
+      . left; use lψ;
+      . simp_all;
+    . replace ih₂ :
+          (f x) ⊧ ψ →
+          (∀ a ∈ Γ.fmls, f ⊧ a) →
+          (∀ a b, (a, b) ∈ Γ.rels → LabelTerm.evaluated f (a, b)) →
+          (∃ lφ ∈ Δ.fmls, f ⊧ lφ) ∨ ∃ a b, (a, b) ∈ Δ.rels ∧ LabelTerm.evaluated f (a, b)
+          := by simpa [Sequent.Satisfies] using ih₂ M f;
+      rcases ih₂ h hΓ₂ hX with _ | _ <;> simp_all;
+  | @impR Γ Δ x φ ψ d ih =>
+    rintro M f ⟨hΓ, hX⟩;
+    suffices ((¬(f x) ⊧ φ ∨ (f x) ⊧ ψ) ∨ ∃ a ∈ Δ.fmls, f ⊧ a) ∨ ∃ a b, (a, b) ∈ Δ.rels ∧ LabelTerm.evaluated f (a, b) by
+      simpa [LabelledFormula.Satisfies.imp_def, Semantics.Imp.realize_imp, imp_iff_not_or];
+    wlog _ : (f x) ⊧ φ;
+    . tauto;
+    replace ih :
+      (f ⊧ x ∶ φ) →
+      (∀ a ∈ Γ.fmls, f ⊧ a) →
+      (∀ a b, (a, b) ∈ Γ.rels → LabelTerm.evaluated f (a, b)) →
+      ((f ⊧ x ∶ ψ) ∨ ∃ a ∈ Δ.fmls, f ⊧ a) ∨ ∃ a b, (a, b) ∈ Δ.rels ∧ LabelTerm.evaluated f (a, b)
+      := by simpa [Sequent.Satisfies] using ih M f;
+    rcases ih (by simpa) (by simpa using hΓ) (by simpa using hX) with (h | _) | _;
+    . tauto;
+    . simp_all;
+    . simp_all;
+  | @boxL Γ Δ x y φ d ih =>
+    rintro M f ⟨hΓ, hX⟩;
+
+    have ⟨hxbφ, hΓ₂⟩ : (f ⊧ x ∶ □φ) ∧ ∀ a ∈ Γ.fmls, f ⊧ a := by simpa using hΓ;
+    have ⟨hxy, hX₂⟩ : LabelTerm.evaluated f (x, y) ∧ ∀ a b, (a, b) ∈ Γ.rels → LabelTerm.evaluated f (a, b) := by simpa using hX;
+    have hyφ : f ⊧ y ∶ φ := Formula.Kripke.Satisfies.box_def.mp (LabelledFormula.Satisfies.box_def.mp hxbφ) _ hxy;
+
+    replace ih :
+      (f ⊧ x ∶ □φ) →
+      (f ⊧ y ∶ φ) →
+      (∀ a ∈ Γ.fmls, f ⊧ a) →
+      LabelTerm.evaluated f (x, y) →
+      (∀ a b, (a, b) ∈ Γ.rels → LabelTerm.evaluated f (a, b)) →
+      (∃ lφ ∈ Δ.fmls, f ⊧ lφ) ∨ ∃ a b, (a, b) ∈ Δ.rels ∧ LabelTerm.evaluated f (a, b) := by simpa [Sequent.Satisfies] using ih M f;
+
+    rcases ih hxbφ hyφ hΓ₂ hxy hX₂ with _ | _ <;> simp_all;
+  | @boxR Γ Δ x y φ hxy hyΓ hyΔ d ih =>
+    rintro M f ⟨hΓ, hX⟩;
+
+    suffices ((f ⊧ x ∶ □φ) ∨ ∃ a ∈ Δ.fmls, f ⊧ a) ∨ ∃ a b, (a, b) ∈ Δ.rels ∧ LabelTerm.evaluated f (a, b) by simpa;
+    apply or_iff_not_imp_right.mpr;
+    intro hΔ₁; push_neg at hΔ₁;
+    apply or_iff_not_imp_right.mpr;
+    intro hΔ₂; push_neg at hΔ₂;
+
+    intro w hw;
+    let g : Label.Assignment M := λ z => if z = y then w else f z;
+
+    replace ih :
+      (∀ lφ ∈ Γ.fmls, g ⊧ lφ) →
+      LabelTerm.evaluated g (x, y) →
+      (∀ a b, (a, b) ∈ Γ.rels → LabelTerm.evaluated g (a, b)) →
+      ((g ⊧ y ∶ φ) ∨ ∃ a ∈ Δ.fmls, g ⊧ a) ∨ ∃ a b, (a, b) ∈ Δ.rels ∧ LabelTerm.evaluated g (a, b)
+      := by simpa [Sequent.Satisfies] using ih M g;
+    have : ∀ lφ ∈ Γ.fmls, g ⊧ lφ := by
+      rintro ⟨a, ψ⟩ hz;
+      have ha : a ≠ y := by
+        rintro rfl;
+        have := SequentPart.not_include_labelledFml_of_isFreshLabel hyΓ ψ;
+        contradiction;
+      simpa [g, ha] using hΓ _ hz
+    have : LabelTerm.evaluated g (x, y) := by simpa [LabelTerm.evaluated, g, hxy];
+    have : ∀ a b, (a, b) ∈ Γ.rels → LabelTerm.evaluated g (a, b) := (by
+      intro a b r;
+      have ha : a ≠ y := by
+        rintro rfl;
+        have := SequentPart.not_include_relTerm_of_isFreshLabel₁ hyΓ b;
+        contradiction;
+      have hb : b ≠ y := by
+        rintro rfl;
+        have := SequentPart.not_include_relTerm_of_isFreshLabel₂ hyΓ a;
+        contradiction;
+      simpa [LabelTerm.evaluated, g, ha, hb] using hX (a, b) r;
+    )
+    rcases ih (by assumption) (by assumption) (by assumption) with (h | ⟨⟨z, ψ⟩, h₁, h₂⟩) | ⟨a, b, h₁, h₂⟩;
+    . simpa [g, LabelledFormula.Satisfies.iff_models] using h;
+    . have hz : z ≠ y := by
+        rintro rfl;
+        have := SequentPart.not_include_labelledFml_of_isFreshLabel hyΔ ψ;
+        contradiction;
+      have :  f ⊧ z ∶ ψ := by simpa [g, hz] using h₂;
+      have : ¬f ⊧ z ∶ ψ := hΔ₂ _ h₁;
+      contradiction;
+    . have ha : a ≠ y := by
+        rintro rfl;
+        have := SequentPart.not_include_relTerm_of_isFreshLabel₁ hyΔ b;
+        contradiction;
+      have hb : b ≠ y := by
+        rintro rfl;
+        have := SequentPart.not_include_relTerm_of_isFreshLabel₂ hyΔ a;
+        contradiction;
+      have :  (f a) ≺ (f b) := by simpa [LabelTerm.evaluated, g, ha, hb] using h₂;
+      have : ¬(f a) ≺ (f b) := hΔ₁ a b h₁;
+      contradiction;
+
+end Gentzen
+
+
+
+end Labelled
+
+end LO.Modal
